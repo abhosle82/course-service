59124e5f50c0746219c4207731773f1c09ba371e
ab1975 <ajaykbhosle@gmail.com>
2022-09-10 19:53:48 +0530

    included openapi for CourseController

--- src/main/java/edu/spring/boot/CourseServiceApplication.java
+++ src/main/java/edu/spring/boot/CourseServiceApplication.java
@@ -1,9 +1,12 @@
 package edu.spring.boot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Course Service",version = "v 3.0",description = "Course service Api Crud operation"))
 public class CourseServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/edu/spring/boot/controller/CourseController.java
+++ src/main/java/edu/spring/boot/controller/CourseController.java
@@ -5,6 +5,11 @@ import edu.spring.boot.dto.CourseResponseDTO;
 import edu.spring.boot.dto.ServiceResponse;
 import edu.spring.boot.entity.Course;
 import edu.spring.boot.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +17,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.security.Provider;
 import java.util.List;
+/*
+http://localhost:8080/v3/api-docs
+http://localhost:8080/v3/api-docs.yaml
+http://localhost:8080/swagger-ui/index.html
+*/
 
 @RestController
 @RequestMapping("/course")
@@ -23,6 +33,14 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    @Operation(summary = "add a new course to the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "course added successfully",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = CourseResponseDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400",description = "validation error")
+    })
     @PostMapping
     public ServiceResponse<CourseResponseDTO> addNewCourse(@RequestBody @Valid CourseRequestDTO courseRequestDTO){
         ServiceResponse<CourseResponseDTO> serviceResponse = new ServiceResponse<>();
@@ -36,27 +54,32 @@ public class CourseController {
         return serviceResponse;
     }
 
+    @Operation(summary = "Fetch all course object")
     @GetMapping
     public ServiceResponse<List<CourseResponseDTO>> findAllCourses(){
         return new ServiceResponse<>(HttpStatus.OK,courseService.viewAllCourses());
     }
 
+    @Operation(summary = "Find course by courseId using path variable")
     @GetMapping("/search/path/{courseId}")
     public ServiceResponse<CourseResponseDTO> findCourseUsingPathVariable(@PathVariable Integer courseId){
         return new ServiceResponse<>(HttpStatus.OK,courseService.findCourseById(courseId));
     }
 
+    @Operation(summary = "Find course by courseId using request param")
     @GetMapping("/search/param")
     public ServiceResponse<CourseResponseDTO> findCourseUsingRequestParam(@RequestParam Integer courseId){
         return new ServiceResponse<>(HttpStatus.OK,courseService.findCourseById(courseId));
     }
 
+    @Operation(summary = "Delete a course")
     @DeleteMapping("/delete/{courseId}")
     public ServiceResponse<?> deleteCourse(@PathVariable Integer courseId){
         courseService.deleteCourse(courseId);
         return new ServiceResponse<>(HttpStatus.OK,"Course with Id:"+courseId+" delete successfully");
     }
 
+    @Operation(summary = "Update a course")
     @PutMapping("/update/{courseId}")
     public ServiceResponse<CourseResponseDTO> updateCourse(@PathVariable Integer courseId, @RequestBody CourseRequestDTO courseRequestDTO){
         return new ServiceResponse<>(HttpStatus.OK,courseService.updateCourse(courseId,courseRequestDTO));
